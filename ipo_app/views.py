fda6ec265de8300b512c45e31b27573075cc95b1
ramuyeligapu3 <yeligapuramu.20.ece@anits.edu.in>
2024-12-01 20:18:22 +0530

    templates updated

--- ipo_app/views.py
+++ ipo_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import IPODetails, User
 
-# Create your views here.
+
+def dashboard(request):
+    return render(request, "ipo_app/dashboard.html")  # Ensure the path to your template is correct
+
--- ipo_project/settings.py
+++ ipo_project/settings.py
@@ -124,7 +124,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "static/"
+# settings.py
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "ipo_app/static",  # Add this line if your static files are not in a default static folder
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
